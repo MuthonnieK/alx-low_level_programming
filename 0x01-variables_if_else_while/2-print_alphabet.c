334fdeb5c3cca60f0289e69720c17aef22c50f5a
MuthonnieK <sonniek558@gmail.com>
2022-09-10 05:46:37 -0700

    This is my first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+int main (void)
+
+{
+
+		int alphabet;
+
+			alphabet= 'a';
+
+				while (alphabet <= 'z')
+
+						{
+
+									putchar(alphabet);
+
+											alphabet++;
+
+												}
+
+					putchar('\n');
+
+						return (0);
+
+}
