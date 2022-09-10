0057fa198f5c2e5d479d29a25ee09c52e45673f8
MuthonnieK <sonniek558@gmail.com>
2022-09-10 05:52:37 -0700

    This is my first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+
+int main (void)
+
+{
+
+		int alphabet, ALPHABET;
+
+			alphabet= 'a';
+
+				ALPHABET ='A';
+
+					while (alphabet <= 'z')
+
+							{
+
+										putchar(alphabet);
+
+												alphabet++;
+
+													}
+
+						putchar('\n');
+
+
+
+							while (ALPHABET <='Z')
+
+									{
+
+												putchar(ALPHABET);
+
+														ALPHABET++;
+
+															}
+
+								putchar('\n');
+
+
+
+									return (0);
+
+}
