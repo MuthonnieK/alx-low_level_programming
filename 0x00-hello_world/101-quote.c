2a12105d438acddc7d9094a135a7801a4723efc2
MuthonnieK <sonniek558@gmail.com>
2022-09-12 16:50:18 -0700

    This is my first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
-  and that piece of art is useful - Dora korpar, 2015-10-19'
- Return: Always 0 (Success)
+/**
+ * main - Entry point
+ * Description: 'and that piece of art is useful - Dora korpar, 2015-10-19'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
