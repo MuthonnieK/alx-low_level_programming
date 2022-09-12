a33b6a3ff5dca778700bb1c47803274cad9b702f
MuthonnieK <sonniek558@gmail.com>
2022-09-11 19:02:08 -0700

    This is my first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,10 +5,10 @@
 int main(void)
 {
    int n = '0';
-   while (n <= '9')
+   while (n <='9')
    {
        putchar(n);
-       if (n != '9')
+       if (n!='9')
         
              putchar(',');
             putchar(' ');
