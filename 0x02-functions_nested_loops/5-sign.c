be305e2d5b9b4edd00576606edb7d58af51bf5b5
MuthonnieK <sonniek558@gmail.com>
2022-09-14 14:29:26 -0700

    This is my first commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,19 @@
  */
 int print_sign(int n)
 {
-	for (n > 0)
+	if (n > 0)
 	{
-		return (1);
 		_putchar ('+');
+		return (1);
 	}
-	for (n < 0)
+	else if (n < 0)
 	{
-		return (-1);
 		_putchar ('-');
+		return (-1);
 	}
-	for (n = 0)
+	else
 	{
+		_putchar ('0');
 		return (0);
-		_putchar (0);
 	}
 }
