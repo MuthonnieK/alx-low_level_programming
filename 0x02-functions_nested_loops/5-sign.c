0e33190b5c756897a46ab04c8bcb3e4fe1149664
MuthonnieK <sonniek558@gmail.com>
2022-09-14 14:03:04 -0700

    This is my first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number.
+ * @n: input of function
+ * Return: 1 and print + if n>0
+ * 0 and print 0 if n=0
+ * -1 and print - if n<0
+ */
+int print_sign(int n)
+{
+	for (n > 0)
+		return (1);
+		_putchar (+);
+	for (n < 0)
+		return (-1);
+		_putchar (-);
+	for (n = 0)
+		return (0);
+		_putchar (0);
+}
