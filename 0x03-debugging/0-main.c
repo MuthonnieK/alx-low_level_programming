c719cf556613887799ad47981c01ea0b1ea6f32b
MuthonnieK <sonniek558@gmail.com>
2022-09-16 12:55:23 -0700

    This is my first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0 always on sucess
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	positive_or_negative(a);
+	return (0);
+}
