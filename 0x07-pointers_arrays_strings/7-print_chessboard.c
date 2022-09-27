4fbb656c02005a4b3e34097a2a8dabaa16e40902
MuthonnieK <sonniek558@gmail.com>
2022-09-27 13:59:56 -0700

    The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - print a chess board
+ * @a: pointer to the elemnt
+ */
+void print_chessboard(char (*a)[8])
+{
+	int m, n;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = 0; n < 8; n++)
+			printf("%", a[m][n]);
+		putchar('\n');
+	}
+}
+
