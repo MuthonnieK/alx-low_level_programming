9b379be3e38f85e4fa776f7d5a6495034dab956e
MuthonnieK <sonniek558@gmail.com>
2022-09-29 13:47:06 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int check_prime(int n, int resp);
+
+/**
+ * is_prime_number - checks whether n is prime
+ * @n: integer
+ * Return: 1 if true, 0 otherwise
+ */
+int is_prime_number(int n);
+{
+	return (check_prime(n, 2));
+}
+/**
+ * check_prime - checks for prime no.
+ * @n: integer
+ * @resp: integer
+ * Return: integer
+ */
+int check_prime(int n, int resp)
+{
+	if (resp >= n && n > 1)
+		return (1);
+	else if (n % resp == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, resp + 1));
+}
