30cd63fd4ac6f850b318ee224a2359667d200d10
MuthonnieK <sonniek558@gmail.com>
2022-10-04 07:12:53 -0700

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: pointer to a pointer
+ * @height: height of grid
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+	free(grid);
+}
